7163acceba1f73c8e8f6a30eca74f4e36e0e4caa
Udon <udoooon0727@gmail.com>
2021-08-26 20:40:15 +0900

    Update 001.sh

--- scripts/train/gan_from_z/001.sh
+++ scripts/train/gan_from_z/001.sh
@@ -1,7 +1,8 @@
-python main.py gpus=[3] \
-    experiment_name=DCGAN_Celeba \
-    versions=001 \
-    solver=src.solver.gan_from_z \
-    dataset=celeba \
-    epochs=5 \
-    optimizer.betas=[0.5,0.999]
+CUDA_VISIBLE_DEVICES=$1 \
+    python main.py gpus=-1 \
+        experiment_name=DCGAN_Celeba \
+        versions=001 \
+        solver=src.solver.gan_from_z \
+        dataset=celeba \
+        epochs=5 \
+        optimizer.betas=[0.5,0.999]
